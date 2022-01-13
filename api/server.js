ed4cc1f76688c37f6b7fa44942c720fbe041f78b
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-13 02:49:14 -0500

    Wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const { logger,
+  validateUserId,
+  validateUser,
+  validatePost, } = require('./middleware')
 const server = express();
 
 server.use(express.json())
+
+server.use(logger)
+
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
